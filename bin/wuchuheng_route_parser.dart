7eccda6d54d13cd226ebcc56d47cec538d558a3f
wuchuheng <root@wuchuheng.com>
2023-01-17 16:44:44 +0800

    init

---
+++ bin/wuchuheng_route_parser.dart
@@ -0,0 +1,5 @@
+import 'package:wuchuheng_route_parser/wuchuheng_route_parser.dart' as wuchuheng_route_parser;
+
+void main(List<String> arguments) {
+  print('Hello world: ${wuchuheng_route_parser.calculate()}!');
+}
---
+++ test/wuchuheng_route_parser_test.dart
@@ -0,0 +1,8 @@
+import 'package:wuchuheng_route_parser/wuchuheng_route_parser.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
